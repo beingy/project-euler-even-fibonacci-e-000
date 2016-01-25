63980844b48f4f5704e395e3231678d1f4e32e69
beingy <beingy@gmail.com>
2016-01-25 17:01:08 +0000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,19 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(value)
+  first_fibo = 0
+  second_fibo = 1
+  current_fibo = 0
+  sum_even_fibos = 0
+  while current_fibo <= value
+    if current_fibo != 0 && current_fibo % 2 == 0
+      #puts "even fibo: #{current_fibo.to_i}"
+      sum_even_fibos = current_fibo + sum_even_fibos
+    end
+    current_fibo = first_fibo + second_fibo
+    first_fibo = second_fibo
+    second_fibo = current_fibo
+  end
+  sum_even_fibos
+end
+
+#even_fibonacci_sum(10)
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,25 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  def initialize(value)
+    @value = value
+  end
+
+  def sum
+    first_fibo = 0
+    second_fibo = 1
+    current_fibo = 0
+    sum_even_fibos = 0
+    while current_fibo <= @value
+      if current_fibo != 0 && current_fibo % 2 == 0
+        puts "Even fibo: #{current_fibo}"
+        sum_even_fibos = current_fibo + sum_even_fibos
+      end
+      current_fibo = first_fibo + second_fibo
+      first_fibo = second_fibo
+      second_fibo = current_fibo
+    end
+    sum_even_fibos
+  end
+end
+
+#EvenFibonacci.new(10).add_even_fibos
